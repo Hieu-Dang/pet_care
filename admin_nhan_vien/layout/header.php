455b5bd75196f4067294734e1bb777663899119e
tovantung <v1forever.alone1@gmail.com>
2020-08-31 16:26:04 +0700

    ff

--- admin_nhan_vien/layout/header.php
+++ admin_nhan_vien/layout/header.php
@@ -15,10 +15,13 @@
 <body>
 	<div class="container-fluid mt-1">
 		<div class="row">
-			<div class="col-md-2" id='admin-header'>
+			<div class="col-md-3" id='admin-header'>
 				<div class="row mt-2">
-					<span class=" col-md-6">Đặng Minh Hiếu</span>
-					<span class="col-md-4 ">Nhân viên</span>
+					<span class=" col-md-6"><?php echo $data_user[0]['full_name'] ?></span>
+					<span class="col-md-4 "><?php if ($data_user[0]['lv']=='1') {
+						echo 'admin';
+					}else{echo 'nhân viên';} ?>	
+					</span>
 					<a href="?controller=proflie" class="col-md-2">
 						<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 						  <path fill-rule="evenodd" d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm-1 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm-3 4c2.623 0 4.146.826 5 1.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-1.245C3.854 11.825 5.377 11 8 11z"/>
