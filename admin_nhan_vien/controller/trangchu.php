38368fed226a07a11051ff4ff4aa9c9398097233
tovantung <v1forever.alone1@gmail.com>
2020-08-31 15:35:58 +0700

    xong controller tranchu f

--- admin_nhan_vien/controller/trangchu.php
+++ admin_nhan_vien/controller/trangchu.php
@@ -3,6 +3,6 @@
 		header('location: ?controller=trangchu');
 	}else{
 		$data_user=$db->get('*','admin_nhanvien',array('id'=>$_SESSION['ss_admin_nhanvien']));
-	}
+	} 	
 	require_once('view/trangchu.php');
 ?>
